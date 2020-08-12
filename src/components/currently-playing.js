b97219be21b8c0e90618a58496c9dba4059a2656
Joe Henson <jd.henson@me.com>
2020-08-12 12:14:51 +0100

    Currently playing Spotify API adjustments

--- src/components/currently-playing.js
+++ src/components/currently-playing.js
@@ -14,12 +14,12 @@ class CurrentlyListening extends React.Component {
   componentDidMount() {
     const proxyurl = "https://cors-anywhere.herokuapp.com/";
     const url = "https://ancient-fjord-66964.herokuapp.com/spotify/getRecentTrack";
+    
     fetch(proxyurl + url)
       .then(response => {
         return response.json();
       })
       .then(json => {
-        console.log(json);
         this.setState({
           fetchedData: json,
           loading: false
@@ -32,24 +32,24 @@ class CurrentlyListening extends React.Component {
     const {fetchedData, loading} = this.state;
 
     return (
-        <div className="flex mb-4 mt-5" id="music-player">
-            <div className="flex lg:w-3/4 md:w-3/4 sm:w-3/4 xs:w-full bg-gray-300 rounded-lg p-5">
-                <div className="w-1/4">
-                    <img id="artwork" src={loading ? Placeholder : fetchedData.artwork_lg} className="w-20 h-20 rounded-md" alt="Image" />
-                </div>
-                <div className="ml-3 lg:w-3/4 md:w-3/4 sm:w-3/4 xs:w-full">
-                    <div className={loading ? "loading rounded-lg" : "rounded-lg"}>
-                      <p className="text-2xl font-semibold" id="song">{loading ? 'Loading...' : fetchedData.song_name}</p>
-                    </div>
-                    <div className={loading ? "loading-dark rounded-lg" : "rounded-lg"}>
-                      <p>{loading ? 'Loading...' : fetchedData.album}</p>
-                    </div>
-                    <div className={loading ? "loading rounded-lg" : "rounded-lg"}>
-                      <p>{loading ? 'Loading...' : fetchedData.artist}<span className="float-right"><FontAwesomeIcon icon={fetchedData.currently_listening == 0 ? 'volume-mute' : 'volume-up'} /></span></p>
-                    </div>
-                </div>
-            </div>
-        </div>
+      <div className="flex mb-4 mt-5">
+          <div className="flex lg:w-1/2 md:w-1/2 sm:w-3/4 xs:w-full bg-gray-300 rounded-lg p-5">
+              <div className="w-1/4 artwork-visibility">
+                  <img id="artwork" src={loading ? Placeholder : fetchedData.artwork_lg} className="w-20 h-20 rounded-md" alt="Image" />
+              </div>
+              <div className="md:ml-3 lg:w-3/4 md:w-3/4 sm:w-3/4 xs:w-full">
+                  <div className={loading ? "loading rounded-lg" : "rounded-lg"}>
+                    <p className="text-xl font-semibold" id="song">{loading ? 'Loading...' : fetchedData.song_name.substring(0, 24)}</p>
+                  </div>
+                  <div className={loading ? "loading-dark rounded-lg" : "rounded-lg"}>
+                    <p>{loading ? 'Loading...' : fetchedData.album}</p>
+                  </div>
+                  <div className={loading ? "loading rounded-lg" : "rounded-lg"}>
+                    <p>{loading ? 'Loading...' : fetchedData.artist}<span className="float-right" aria-label={fetchedData.currently_listening == 0 ? "What I was last listening to" : "What I'm listening to right now"} data-balloon-pos="right"><FontAwesomeIcon icon={fetchedData.currently_listening == 0 ? 'volume-mute' : 'volume-up'} /></span></p>
+                  </div>
+              </div>
+          </div>
+      </div>
     )
   }
 
