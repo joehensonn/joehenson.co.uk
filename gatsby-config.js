f9c8ab9060180b424ba5643dfa9e1a70bc7956ae
Joe Henson <jd.henson@me.com>
2020-08-22 10:34:39 +0100

    Google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,47 +4,56 @@ const tailwindConfig = require("./tailwind.config.js");
 const fullConfig = resolveConfig(tailwindConfig);
 
 module.exports = {
-  siteMetadata: {
-    title: `Joe Henson`,
-    description: `A brief overview of me and my work`,
-    author: `@joehenson`,
-  },
-  plugins: [
-    `gatsby-plugin-eslint`,
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-tailwind`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: fullConfig.theme.colors.white,
-        theme_color: fullConfig.theme.colors.teal["400"],
-        display: `minimal-ui`,
-        icon: `src/images/tailwind-icon.png`,
-      },
+    siteMetadata: {
+        title: `Joe Henson`,
+        description: `A brief overview of me and my work`,
+        author: `@joehenson`,
     },
-    {
-      resolve: `gatsby-plugin-postcss`,
-      options: {
-        postCssPlugins: [
-          require(`tailwindcss`)(tailwindConfig),
-          require(`autoprefixer`),
-          ...(process.env.NODE_ENV === `production`
-            ? [require(`cssnano`)]
-            : []),
-        ],
-      },
-    },
-    `gatsby-plugin-offline`,
-    {
-      resolve: 'gatsby-plugin-web-font-loader',
-      options: {
-        google: {
-          families: ['Josefin Sans']
-        }
-      }
-    },
-    `gatsby-plugin-favicon`,
-  ],
+    plugins: [
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                // The property ID; the tracking code won't be generated without it
+                trackingId: "UA-147033707-1",
+                // Defines where to place the tracking script - `true` in the head and `false` in the body
+                head: true,
+            },
+        },
+        `gatsby-plugin-eslint`,
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `gatsby-starter-tailwind`,
+                short_name: `starter`,
+                start_url: `/`,
+                background_color: fullConfig.theme.colors.white,
+                theme_color: fullConfig.theme.colors.teal["400"],
+                display: `minimal-ui`,
+                icon: `src/images/tailwind-icon.png`,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-postcss`,
+            options: {
+                postCssPlugins: [
+                    require(`tailwindcss`)(tailwindConfig),
+                    require(`autoprefixer`),
+                    ...(process.env.NODE_ENV === `production`
+                        ? [require(`cssnano`)]
+                        : []),
+                ],
+            },
+        },
+        `gatsby-plugin-offline`,
+        {
+            resolve: "gatsby-plugin-web-font-loader",
+            options: {
+                google: {
+                    families: ["Josefin Sans"],
+                },
+            },
+        },
+        `gatsby-plugin-favicon`,
+    ],
 };
