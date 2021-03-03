5c9b724d905f29792dc3ff9e7e8a538daad5b225
Joe Henson <jd.henson@me.com>
2021-03-03 16:59:51 +0000

    Remove missing image

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,20 +2,14 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import abductionIllustration from "../images/abduction-illustration.svg";
 
 function NotFoundPage() {
   return (
     <Layout>
       <SEO title="Page Not Found" />
       <div className="text-center">
-        <img
-          alt="Ghost getting abducted by aliens"
-          className="block mx-auto w-1/2"
-          src={abductionIllustration}
-        />
-        <h2 className="bg-yellow-400 text-2xl font-bold inline-block my-8 p-3">
-          Looks like this page is a ghost that got abducted by aliens...
+        <h2 className="text-2xl font-bold inline-block my-8 p-3">
+          Urr... how did you get here?
         </h2>
       </div>
     </Layout>
