cf8fe2daddb985a0974b8a72bdfff555877a29a0
Joe Henson <jd.henson@me.com>
2022-03-22 19:54:54 +0000

    Wrap app in ThemeProvider for darkmode

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,17 +4,24 @@ import React from "react";
 import Header from "./header";
 import Footer from '../components/footer';
 
+import { ThemeProvider } from './theme-context';
+
 function Layout({ children }) {
   return (
-    <div className="flex flex-col min-h-screen font-sans">
-      <Header />
+    <>
+      <ThemeProvider>
+        <div className="flex flex-col min-h-screen font-sans dark:bg-gray-900">
+          <Header />
 
-      <main className="main-content flex-1 justify-center w-full max-w-4xl px-16 py-8 mx-auto md:px-8 md:py-16">
-        {children}
-      </main>
+          <main className="main-content flex-1 justify-center w-full max-w-4xl px-16 py-8 mx-auto md:px-8 md:py-16">
+            {children}
+          </main>
 
-      <Footer />
-    </div>
+          <Footer />
+        </div>
+      </ThemeProvider>
+    </>
+    
   );
 }
 
