59397554cd0f702b35fc96a93e6057e4f68e14f6
Joe Henson <jd.henson@me.com>
2022-08-04 15:46:45 +0100

    Tailwind update for Daisy UI and purging content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 // See https://tailwindcss.com/docs/configuration for details
 module.exports = {
+  content: ['./src/**/*.js'],
   darkMode: 'class',
-  purge: [
-    './src/**/*.js',
-  ],
   theme: {},
   variants: {},
   // https://github.com/tailwindcss/custom-forms
-  plugins: [require("@tailwindcss/custom-forms")],
+  plugins: [require("@tailwindcss/custom-forms"), require("daisyui")],
+  daisyui: {
+    themes: false,
+  },
 };
